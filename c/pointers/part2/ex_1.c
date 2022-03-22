777dfe1ab3842201cb17347541bed8ddd563a0a4
amit.regev <amitregev88@gmail.com>
2022-03-22 08:25:21 +0200

    update ex_1 pointers part1

--- c/pointers/part2/ex_1.c
+++ c/pointers/part2/ex_1.c
@@ -1,6 +1,18 @@
+ /*Strlen*/
+size_t Strlen(const char *s)
+{
+    size_t count = 0;
+    while(*s!='\0')
+    {
+        count++;
+        s++;
+    }
+    return count;
+}
+ 
  
 /*copy char arr sorce  to char arr2 destination */
-Strcpy(char *dest, const char *src, size_t)
+char* Strcpy(char *dest, const char *src)
 {
 	size_t i;
 
@@ -17,7 +29,7 @@ Strcpy(char *dest, const char *src, size_t)
 
 /*copy string to char arr */
 
-Strncpy(char *dest, const char *src, size_t n)
+char* Strncpy(char *dest, const char *src, size_t n)
 {
 	size_t i;
 
@@ -27,22 +39,99 @@ Strncpy(char *dest, const char *src, size_t n)
 		dest[i] = '\0';
 
 	return dest;
-----------------------------------------------------------------	
-	*/
-	int StrCmp (const char *str1, const char *str2)
-{
-	size_t n = 0;
 	
-	while ( (str1[n] != '\0') && (str2[n] != '\0') )
-	{
-		if (str1[n] != str2[n])
+/*compare between str1 and str2 for n characters */
+	
+int Strncmp(const char *str1, const char *str2 , size_t n)
+{
+	size_t i = 0;
+	
+    for (i = 0; i < n && str1[i] && str2[i] != '\0'; i++)
+    {
+		if (str1[i] != str2[i])
 		{
-			return (str1[n] - str2[n]);
+			return (str1[i] - str2[i]);
 		}
-		else n++;
-		
+	}
+return (str1[i] - str2[i]);
+}
+
+/*compare between str1 and str2 -  case-insensitively */
+
+int Strcasecmp(const char *s1, const char *s2)
+{
+	int res = 0;
+	while (*s1 != '\0' && *s2 !='\0')
+    {
+	res = *s1 - *s2;
+	if ((*s1 >= 65 && *s1 <= 90) || (*s1 >= 97 && *s1 <= 122))//checking if s1 between a-z or A-Z
+    {
+		if (res != 0 && res != 32 && res != -32) // this line checks if is not it the smame letter (ex- a-A)
+		{ 
+                return res;
+        }
+    }
+	else if (res != 0) // s1 isn't alphabet. this condition checks if s1 and s2 are diffrent? 
+	{
+		return res;
+	}
+		s1++;
+		s2++;    
+    }
+            
+    return *s1 - *s2; 
+   
+}
+
+
+/*The function  searches the occurrence of a specified character in the given string and returns the pointer to it.*/
+
+
+char *StrChr(const char *str, int c)
+{
+    while (*str!='\0')
+        {
+            if (*str == (char)c)
+                return (char*)str;
+            str++;
+        }    
+    return NULL;
+}
+
+/*The function strdup() is used to duplicate a string. */
+char *Strdup(const char *src)
+{
+    char *str = NULL;
+    size_t len = Strlen(src);
+    
+    str = (char *)malloc(len * sizeof(char));
+    return Strcpy (str, src); 
+    
+}
+
+/*the Strcat() function contcatenates (joins) two strings.*/
+
+
+char *Strcat(char *dest, const char *src)
+{
+	size_t dest_len = Strlen (dest);
+	char* ptr = dest + dest_len; // make `ptr` point to the end of the dest string
+	
+	while (*src != '\0') // add characters of the src to the dest string
+	{
+		* ptr++ = * src++;
 	}
 	
-	return (str1[n] - str2[n]); 
+	*ptr = '\0' ;
+	return dest;
+}
+
+
+
+
+
+
 	
+	
+
 
