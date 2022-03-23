33b41d0a9601c63f5afcb63329893860fdbdc187
amit.regev <amitregev88@gmail.com>
2022-03-23 23:23:30 +0200

    update exrsises of arrays_and_pointer_to_pointer worksheet

--- c/arrays_and_pointer-to-pointer/josepjus.c
+++ c/arrays_and_pointer-to-pointer/josepjus.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *update_arr(int *arr, size_t len);
+/*int *update_arr(int *arr, size_t len);*/
 void josephus(int *arr, size_t n, size_t k);
 
 int main()
 {
 	size_t n = 7;
 	size_t k = 3;
-	size_t i=0;
-
-/*	scanf("num" &n)*/
+	size_t i = 0;
+	
+	/*printf("Enter the number of soldiers (mimimun 2 soldiers)\n");*/
+	/*scanf("%lu", &n);*/
+	/*printf("choose a number of soldier to die first\n");*/
+	/*scanf("%lu", &k);*/
+	
 	int *arr=(int*)malloc(n*sizeof(int));
 /*	k= random (1,n)*/
 
 	
 	if(arr == NULL)
 	{
-		 printf("memory eror");
+		 printf("memory error");
 		 return 1;
 	}		 
 		 
-	for(; i < n; i++) *(arr+i) = i+1;
+	for(; i < n; i++) *(arr + i) = i + 1;
 
 	josephus(arr, n, k);
-
+	free(arr);
+	arr = NULL;
 	return 0;
 }
 
 
 void josephus(int *arr, size_t n, size_t k)
 {
+	
 	size_t i = 0;
+	size_t j = 0;
 	
 	while(n!=1)
 	{
-		i = (i + k-1)%n;
-		if (*(arr +i) != 0) *(arr+i) = 0; 
+		i = (i + k - 1)%n;
 		n--;
-		arr = update_arr(arr, n);
+		if (*(arr + i) != 0)
+		{
+			*(arr + i) = 0; 
+			for(j = i;j < n; j++)
+				{
+				  arr[j] = arr[j + 1];
+				}
+			arr[n] = (int) NULL;
+		}
 	}
 	
-	printf("winer %d", *arr);
+	printf("Soldier No. %d survived in index %d\n", *arr, *arr - 1);
 	
 	/*return arr*/;
 }
 
-
+/*
 int *update_arr(int *arr, size_t len)
 {
 	size_t i=0;
@@ -64,3 +78,4 @@ int *update_arr(int *arr, size_t len)
 	return new;
 
 }
+*/
