d298fc92fa8ff0e681e0e7869bad76cbfd60a17c
root <amit.regegv@gmail.com>
2022-04-12 18:22:17 +0300

    i

---
+++ ds/src/stack.c
@@ -0,0 +1,25 @@
+
+struct stack
+{
+    size_t size;
+    void *stack;
+    size_t capacity;
+    size_t size_of_element;
+};
+
+
+typedef struct stack stack_ty;
+
+stack_ty *StackCreate(size_t capacity, size_t size_of_element)
+
+stack_ty * 
+	
+	stack_ty * = (stack_ty *) malloc(sizeof 
+
+{
+
+
+
+
+
+}
