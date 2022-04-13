c6ce865b1bdbd5b27476631b7156528992f39c19
amit.regev <amitregev88@gmail.com>
2022-04-13 20:47:38 +0300

    ds

--- ds/src/stack.c
+++ ds/src/stack.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 
 struct stack
 {
@@ -10,16 +13,119 @@ struct stack
 
 typedef struct stack stack_ty;
 
+
+stack_ty *StackCreate(size_t capacity, size_t size_of_element);
+void StackDestroy(stack_ty *ptr);
+void StackPush(stack_ty* ptr, void *element);
+void StackPop(stack_ty* ptr);
+
+
+
+
 stack_ty *StackCreate(size_t capacity, size_t size_of_element)
-
-stack_ty * 
+{
 	
-	stack_ty * = (stack_ty *) malloc(sizeof 
+	stack_ty *struct_stack = (stack_ty *) malloc (sizeof(stack_ty)); /* memory allocation for the struct of stack*/
+	
+	if (struct_stack == NULL)
+	{
+		printf("error of memory allocation\n");  
+		return NULL;
+	}
+	else if (capacity == 0)
+	{
+		free(struct_stack);
+		return NULL;
+	}
+	
+	struct_stack->size = 0;
+	struct_stack->capacity = capacity;
+	struct_stack->size_of_element = size_of_element;
+	struct_stack->stack = (void *) malloc (size_of_element * capacity); /* memory allocation for the stack*/
+	if (struct_stack->stack == NULL)
+	{
+		printf("error of memory allocation\n");  
+		return NULL;
+	}
+	
+	
+	return struct_stack;
+} 
 
+
+void StackDestroy(stack_ty *ptr)
+{
+	if (*ptr.stack != NULL)
+	{
+		free(*ptr.stack);
+	}
+	
+	if(ptr != NULL)
+	{
+		free(ptr);
+	}
+	
+}
+
+void StackPush(stack_ty* ptr, void *element)
+{
+	if (ptr == NULL)
+	{
+		printf("Memory allocation error\n");
+	}
+	
+	if (*ptr.size == *ptr.capacity)
+	{
+		printf("The stack is full . unable to push element to stack\n");
+	}	
+	
+	
+	memcpy((char *)ptr->stack + ptr->size * ptr->size_of_element, element, ptr->size_of_element);
+	
+	++ptr->size;
+	
+}
+
+
+
+void StackPop(stack_ty* ptr)
+{
+	if (ptr->size == 0)
+	{
+		return;
+		
+	}
+	
+	--ptr->size;
+	
+	
+}
+
+
+const void *StackPeek(const stack_ty* ptr)
+{
+	if (ptr == NULL)
+	{	
+		printf("memory allocation of stack error\n");
+		return NULL;
+	}
+			
+	ptr
+
+
+}
+
+int main()
 {
 
 
 
 
 
+
 }
+
+
+
+
+
---
+++ ds/test/stack.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include "../include/bit_array.h"
+
+
+typedef struct stack stack_ty;
+
+
+void TestStackCreate(void);
+void TestStackDestroy(void);
+void Test_StackPush_StackSize_StackPeek(void)
+void TestStackPop(void);
+
+
+
+
+
+
+/************************TestStackCreateAndDestroy**********************/
+
+
+void TestStackCreateAndDestroy(void)
+{
+
+	stack stack_ty *ptr = NULL;
+
+/*create a stack*/
+
+	ptr = StackCreate(10, 11); /*checking in case the size of element greater than capacity */ 
+	
+	if (ptr == NULL)
+	{
+		printf("Test create stack succeed - in case size of elements greater than capacity\n");
+	}
+	else
+	{
+		printf("Test create failed - in case size of elements greater than capacity\n");
+		StackDestroy(ptr);	/*destroy a stack - test*/
+	
+		if (ptr == NULL)
+		{
+			printf("Test destroy stack succeed in case the size of elements greater than capacity\n");
+		}
+		else
+		{
+			printf("Test destroy stack failed in case  the size of elements greater than capacity \n");
+		}
+	}
+	
+	
+/*create a stack again*/
+	ptr = NULL;
+	
+	ptr = StackCreate(15, 2); /*checking in case the capacity value  greater than size of elements */ 
+	if (ptr != NULL)
+	{
+		printf("Test create stack succeed - in case the capacity greater than size of elements\n");
+		StackDestroy(ptr);	/*destroy a stack - test*/
+		if (ptr == NULL)
+		{
+			printf("Test destroy stack succeed - in case the capacity greater than size of elements\n");
+		}
+		else
+		{
+			printf("Test destroy stack failed - in case the capacity greater than size of elements\n");
+		}
+	}
+	else
+	{
+		printf("Test create stack failed - in case the capacity greater than size of elements\n");
+	}
+
+}
+
+void Test_StackPush_StackSize_StackPeek(void)
+{
+	
+	char  a = 'a';
+
+	stack_ty *ptr = StackCreate(1,1); 
+	
+	StackPush(ptr, &a)
+		
+	if (StackSize(ptr)==1)
+	{
+		printf("succeed stack push\n");
+		
+	}
+	else 
+	{
+	printf("failed stack push\n");
+	
+	
+	}
+	
+	
+	if (*(char *)(StackPeek(ptr) == a)
+	{
+		printf("succed stackpeek\n");
+	}
+	
+	else 
+	{
+	
+		printf("failed stackpeek\n");
+	}
+	
+	
+	SrackPop(ptr)
+	
+	if (size == 0)
+	
+	{
+		printf("succed StackPop\n");
+	}
+	
+	else 
+	{
+	
+		printf("failed stackPop");
+	}
+}
+
+
+
+
+
+
+
+
+}
+
+	/*
+struct stack
+{
+    size_t size;
+    void *stack;
+    size_t capacity;
+    size_t size_of_element;
+};
+*/
+
+
+
