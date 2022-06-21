9f0c02c9cb79d719bf3a4e6503d7d0fff8918115
amit.regev <amitregev88@gmail.com>
2022-06-21 20:12:22 +0300

    calculator

--- ds/calculator/calculator.c
+++ ds/calculator/calculator.c
@@ -16,7 +16,7 @@
 
 
 
-typedef void (*FuncPtr_ty)(stack_ty *output_stack);
+typedef void (*FuncPtr_ty)(stack_ty *operand_stack);
 
 typedef struct operator
 {
@@ -25,61 +25,62 @@ typedef struct operator
 }operator_ty;
 
 
-typedef void (*action_ty)(stack_ty *output_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p);
+typedef void (*action_ty)(stack_ty *operand_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p);
 
-static void add(stack_ty *output_stack);
-static void sub(stack_ty *output_stack);
-static void mult(stack_ty *output_stack);
-static void divide(stack_ty *output_stack);
+static void add(stack_ty *operand_stack);
+static void sub(stack_ty *operand_stack);
+static void mult(stack_ty *operand_stack);
+static void divide(stack_ty *operand_stack);
 
-static void PushToOutputStack(stack_ty *output_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p);
-static void CalcSubExp(stack_ty *output_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p);
+static void PushToOutputStack(stack_ty *operand_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p);
+static void CalcSubExp(stack_ty *operand_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p);
+
+static void OpenParanthesis(stack_ty *operand_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p);
+static void CloseParanthesis(stack_ty *operand_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p);
 
 static char *GetNextChar(char *s);
-
+static int WhatParanthesIs(char *iter);
 
 double calculator(const char *expression)
 {
 
-    stack_ty *outputstack = NULL;
+    stack_ty *operandstack = NULL;
 
     stack_ty *operatorstack = NULL;
-
-    size_t i = 0;
-
-    int index_act = 0;
 
     size_t exp_length = strlen(expression);
 
     operator_ty LutOper[128];
 
-    action_ty LutAct[2] = {&PushToOutputStack, &CalcSubExp};
-
-    double output_exp_num[50] = {0};
+    action_ty LutAct[4] = {&PushToOutputStack, &CalcSubExp, &OpenParanthesis, &CloseParanthesis};
 
     double result = 0;
 
     char *iter_exp = NULL;
 
-    char *iter_operator = NULL;
-
     char user_exp[100];
 
-    LutOper['+'].priority = 1;
+    LutOper['+'].priority = 3;
     LutOper['+'].func = add;
 
-    LutOper['-'].priority = 1;
+    LutOper['-'].priority = 3;
     LutOper['-'].func = sub;
  
-    LutOper['*'].priority = 2;
+    LutOper['*'].priority = 4;
     LutOper['*'].func = mult;
 
-    LutOper['/'].priority = 2;
+    LutOper['/'].priority = 4;
     LutOper['/'].func = divide;
 
-    outputstack = StackCreate(exp_length,sizeof(double));
+    LutOper['('].priority = 1;
+    LutOper['('].func = NULL;
+
+    LutOper[')'].priority = 2;
+    LutOper[')'].func = NULL;
+
+    operandstack = StackCreate(exp_length,sizeof(double));
     
-    if (outputstack == NULL) 
+    if (operandstack == NULL) 
     {
         return -1;
     }
@@ -88,27 +89,21 @@ double calculator(const char *expression)
 
     if (operatorstack == NULL) 
     {
-        StackDestroy(outputstack);
+        StackDestroy(operandstack);
         return -1;
     }
-   
    
     strcpy(user_exp, expression);
     iter_exp = GetNextChar(user_exp);
 
-   
     while(*iter_exp != '\0' && *iter_exp != '\n') /* checks if iter_exp is num or operator*/
-    {      
-       
-        int index_act = !isdigit(*iter_exp);
+    {         
+        int index_act = !isdigit(*iter_exp) + WhatParanthesIs(iter_exp);
 
-        LutAct[index_act](outputstack, operatorstack, LutOper, &iter_exp); 
+        LutAct[index_act](operandstack, operatorstack, LutOper, &iter_exp); 
         
         iter_exp = GetNextChar(iter_exp);
-
     }
-
-
 
     while(!StackIsEmpty(operatorstack))
     {
@@ -117,28 +112,32 @@ double calculator(const char *expression)
         index_operator = (int)*(char *)StackPeek(operatorstack);
         StackPop(operatorstack);
 
-        LutOper[index_operator].func(outputstack);
+        LutOper[index_operator].func(operandstack);
         
     }
 
-    result = *(double *)StackPeek(outputstack);
+    result = *(double *)StackPeek(operandstack);
 
-    StackDestroy(outputstack);
+    StackDestroy(operandstack);
     StackDestroy(operatorstack);
 
     return result;
 }
 
-static void PushToOutputStack(stack_ty *output_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p)
+static void PushToOutputStack(stack_ty *operand_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p)
 {
     double number[10] = {0};
 
     number[0] = strtod(*p, p);
         
-    StackPush(output_stack, &number[0]);
+    StackPush(operand_stack, &number[0]);
+
+    operator_stack = operator_stack;
+    LutOper = LutOper;
+
 }  
 
-static void CalcSubExp(stack_ty *output_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p)
+static void CalcSubExp(stack_ty *operand_stack,stack_ty *operator_stack, operator_ty LutOper[], char **p)
 {
         
     /* checks if operator_stack is not empty and the priority of given operator less than (or equal to)  operator in head of operator_stack */
@@ -146,7 +145,8 @@ static void CalcSubExp(stack_ty *output_stack,stack_ty *operator_stack, operator
     while (!StackIsEmpty(operator_stack) && LutOper[(int)**p].priority <= LutOper[(int)*(char *)StackPeek(operator_stack)].priority)
     {
         size_t index_operator = (int)*(char *)StackPeek(operator_stack);
-        LutOper[index_operator].func(operator_stack);
+        LutOper[index_operator].func(operand_stack);
+        StackPop(operator_stack);
     }
     
     StackPush(operator_stack, *p);
@@ -154,54 +154,84 @@ static void CalcSubExp(stack_ty *output_stack,stack_ty *operator_stack, operator
 }
 
 
-static void add(stack_ty *output_stack)
+static void OpenParanthesis(stack_ty *operand_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p)
+{
+    StackPush(operator_stack, *p);
+    ++*p;
+
+    operand_stack = operand_stack;
+    
+    LutOper = LutOper;
+}
+
+static void CloseParanthesis(stack_ty *operand_stack, stack_ty *operator_stack, operator_ty LutOper[], char **p)
+{
+    int operator_peek = 0;
+
+    operator_peek = (int)*(char *)StackPeek(operator_stack);
+
+    while(operator_peek != '(')
+    {
+        LutOper[operator_peek].func(operand_stack);
+
+        StackPop(operator_stack);
+
+        operator_peek = (int)*(char *)StackPeek(operator_stack);
+    }
+
+    StackPop(operator_stack);
+    ++*p;
+}
+
+
+static void add(stack_ty *operand_stack)
 {
     double *a = NULL , *b = NULL;
     
-    a = (double *)StackPeek(output_stack);
+    a = (double *)StackPeek(operand_stack);
 
-    StackPop(output_stack);
+    StackPop(operand_stack);
 
-    b = (double *)StackPeek(output_stack);
+    b = (double *)StackPeek(operand_stack);
 
     *b += *a;
 }
 
-static void sub(stack_ty *output_stack)
+static void sub(stack_ty *operand_stack)
 {
     double *a = NULL , *b = NULL;
     
-    a = (double *)StackPeek(output_stack);
+    a = (double *)StackPeek(operand_stack);
 
-    StackPop(output_stack);
+    StackPop(operand_stack);
 
-    b = (double *)StackPeek(output_stack);
+    b = (double *)StackPeek(operand_stack);
 
     *b -= *a;
 }
 
-static void mult(stack_ty *output_stack)
+static void mult(stack_ty *operand_stack)
 {
     double *a = NULL , *b = NULL;
     
-    a = (double *)StackPeek(output_stack);
+    a = (double *)StackPeek(operand_stack);
 
-    StackPop(output_stack);
+    StackPop(operand_stack);
 
-    b = (double *)StackPeek(output_stack);
+    b = (double *)StackPeek(operand_stack);
 
     *b *= *a;
 }
 
-static void divide(stack_ty *output_stack)
+static void divide(stack_ty *operand_stack)
 {
     double *a = NULL , *b = NULL;
     
-    a = (double *)StackPeek(output_stack);
+    a = (double *)StackPeek(operand_stack);
 
-    StackPop(output_stack);
+    StackPop(operand_stack);
 
-    b = (double *)StackPeek(output_stack);
+    b = (double *)StackPeek(operand_stack);
 
     *b /= *a;
 }
@@ -216,3 +246,20 @@ static char *GetNextChar(char *s)
     return s;
 }
 
+static int WhatParanthesIs(char *iter)
+{
+    while ((int)*iter == '(')
+    {
+        return 1;
+
+    }
+
+
+    while ((int)*iter == ')')
+    {
+        return 2;
+
+    }
+
+    return  0;
+}
--- ds/calculator/calculator_test.c
+++ ds/calculator/calculator_test.c
@@ -7,12 +7,12 @@ int main()
     char *exp1 = "13-3";/* 10.0 */
     char *exp2 = "13-3.14*2";/* 6.72 */
     char *exp3 = "1336.137/12.5434/437.43";/* 0.2435 */
-    /*char *exp4 = "-46.342+4637/34/2.4";/* 10.483980392 */
+    char *exp4 = "5 * (2 * 4 + 3)"; /* 55.0 */
     
-    printf("result of the expression 1 is: %.01f\n", calculator(exp1));
-    printf("result of the expression 2 is: %.02f\n", calculator(exp2));
-    printf("result of the expression 3 is: %.04f\n", calculator(exp3));
-    /*printf("result of the expression 4 is: %.04f\n", calculator(exp4));*/
+    printf("result of the expression 1 (10.0) is: %.01f\n", calculator(exp1));
+    printf("result of the expression 2 (6.72) is: %.02f\n", calculator(exp2));
+    printf("result of the expression 3 (0.2435) is: %.4f\n", calculator(exp3));
+    printf("result of the expression 4 (55.0) is: %.1f\n", calculator(exp4));
     
     return 0;
 }
