f826080179ae0052e066332b54a3ee6fd19b074d
amit.regev <amitregev88@gmail.com>
2022-07-27 13:47:23 +0300

    ping pong

--- system_programming/signals_ping_pong/ex4/ping/ping.c
+++ system_programming/signals_ping_pong/ex4/ping/ping.c
@@ -3,23 +3,32 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/signalfd.h>
+
 
 /*#include <siginfo.h>*/
 
 static int sig_arrived = 0;
+pid_t ping_pid = getpid();
 
-void signal_handler1(int signum, siginfo_t *, void *)
+void signal_handler_ping(int signum, siginfo_t *sinfo, void *param)
 {
+
+    sinfo->si_pid = ping_pid;
+
+    printf("Pong\n");
     sig_arrived = 1;
+    
+    
     /*kill(getppid(),SIGUSR1); */
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
     struct sigaction sa;
-    sa.sa_sigaction =  signal_handler1;
+    sa.sa_sigaction =  signal_handler_ping;
      
 
     sigaction(SIGUSR1, &sa, NULL);
@@ -28,13 +37,12 @@ int main()
     {
         while (sig_arrived == 0)
         {
-            puts("Pong\n");
-            sleep(100);
+            sleep(10);
         }
 
         sig_arrived = 0;
 
-        kill(sa.sa, SIGUSR2);
+        kill(atoi(argv[1]), SIGUSR2);
     }
 
     return 0;
