5cc0a0f00a89704e469653861656473b6f66705a
amit.regev <amitregev88@gmail.com>
2022-08-21 18:33:00 +0300

    ex4-100Kthreads

--- system_programming/100k_threads/ex4.c
+++ system_programming/100k_threads/ex4.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LONG_NUM 5000000000
+#define LONG_NUM 5000000000UL
 
 typedef struct range
 {
@@ -19,12 +19,11 @@ void *Sum_Of_Divisors(void *param)
     size_t sum_of_divisors = 0;
     size_t i = 0;
     
-    for(i= range->from; i< range->to ;++i)
+    for(i= range->from; i<= range->to ;++i)
     {
         if(0 == LONG_NUM % i)
         {
-            sum_of_divisors+=i;
-
+            sum_of_divisors += i;
         }
     }
 
@@ -39,19 +38,14 @@ int main (int argc ,char **argv)
     range_ty *ranges;
     time_t start , end ;
     size_t sum =0;
-    size_t ret_val = 0;
-    size_t num_threads = 2;
+    void *ret_val = NULL;
+    size_t num_threads = 1;
 
-    /*if(argc <= 2)
+
+    if(argc > 1)
     {
-        num_threads = 1;
-
-    }
-    else
-    {
-
         num_threads = atoi(argv[1]);
-    }*/
+    }
     
     threads = malloc(num_threads * sizeof(pthread_t));
     if(!threads)
@@ -72,33 +66,28 @@ int main (int argc ,char **argv)
         ranges[i].from = LONG_NUM / num_threads * i + 1;
         ranges[i].to = LONG_NUM / num_threads * (i+1);
 
-
         if(pthread_create(&threads[i],NULL,Sum_Of_Divisors,&ranges[i])!= 0)
         {
-            printf("thread create failed %ld",i);
+            printf("thread create failed %ld\n",i);
         }
     }
   
     for(i=0; i<num_threads; ++i)
     {
 
-        if(pthread_join(threads[i], (void **)&ret_val) != 0)
+        if(pthread_join(threads[i], &ret_val) != 0)
         {
             printf("thread create failed %ld\n",i);
         }
+        sum += (size_t)ret_val;
     }
 
-    sum += ret_val;
-    
-
-        
+            
     end = time(NULL);
 
-    printf(" took  %ld sec\n",end-start);
+    printf("took  %ld sec\n",end-start);
 
-    printf("Sum Of Divisors %ld\n",sum);
+    printf("Sum Of Divisors %lu\n",sum);
 
     return 0; 
-
-
 }
